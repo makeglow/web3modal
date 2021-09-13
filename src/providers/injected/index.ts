c4af685822772fa6cea700a65c333b6099e957a8
Adam Gałązkiewicz <a@galazkiewicz.com>
2021-09-13 13:20:05 +0200

    new provider - coin98

--- src/providers/injected/index.ts
+++ src/providers/injected/index.ts
@@ -28,6 +28,8 @@ import OperaLogo from "../logos/opera.svg";
 import FrameLogo from "../logos/frame.svg";
 // @ts-ignore
 import LiqualityLogo from "../logos/liquality.png";
+// @ts-ignore
+import Coin98Logo from "../logos/coin98.png";
 
 export const FALLBACK: IProviderInfo = {
   id: "injected",
@@ -139,4 +141,12 @@ export const LIQUALITY: IProviderInfo = {
   logo: LiqualityLogo,
   type: "injected",
   check: "isLiquality"
+};
+
+export const COIN98: IProviderInfo = {
+  id: "injected",
+  name: "Coin98",
+  logo: Coin98Logo,
+  type: "injected",
+  check: "isCoin98"
 };
